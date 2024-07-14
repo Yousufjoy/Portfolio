2b49f514ebade425c75944cfef4cb3c2a4f78050
yousufjoy <yousufj93006@gmail.com>
2024-07-15 00:20:58 +0600

    update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,18 +1,16 @@
-
 import logo from "../assets/logo.jpg";
 
 import { FaGithub, FaFacebookF, FaLinkedinIn } from "react-icons/fa";
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 
 const Navbar = () => {
-
   return (
     <motion.nav
       initial={{ y: -250 }}
       animate={{ y: 0 }}
       transition={{ delay: 0.2, duration: 0.5, type: "spring", stiffness: 20 }}
-
-      className="p-3 flex bg-white justify-between items-center  md:px-[120px]">
+      className="p-3 flex bg-white justify-between items-center  md:px-[120px]"
+    >
       <a href="#" id="brand" className="flex gap-2 items-center">
         <img className="object-cover max-w-[80px] max-h-[80px]" src={logo} />
         {/* <span className="flex font-bold font-display">Portfolio</span> */}
@@ -23,11 +21,10 @@ const Navbar = () => {
             scale: 1.1,
             textShadow: "0px 0px 8px rgb(255,255,255)",
             boxShadow: "0px 0px 8px rgb(255,255,255)",
-
           }}
-
           href="#"
-          className="font-medium hover:text-primary">
+          className="font-medium hover:text-primary"
+        >
           Home
         </motion.a>
         <motion.a
@@ -35,9 +32,10 @@ const Navbar = () => {
             scale: 1.1,
             textShadow: "0px 0px 8px rgb(255,255,255)",
             boxShadow: "0px 0px 8px rgb(255,255,255)",
-
           }}
-          href="#" className="font-medium hover:text-primary">
+          href="#"
+          className="font-medium hover:text-primary"
+        >
           Project
         </motion.a>
 
@@ -46,10 +44,10 @@ const Navbar = () => {
             scale: 1.1,
             textShadow: "0px 0px 8px rgb(255,255,255)",
             boxShadow: "0px 0px 8px rgb(255,255,255)",
-
           }}
-
-          href="#" className="font-medium hover:text-primary">
+          href="#"
+          className="font-medium hover:text-primary"
+        >
           Skill
         </motion.a>
         <motion.a
@@ -57,27 +55,30 @@ const Navbar = () => {
             scale: 1.1,
             textShadow: "0px 0px 8px rgb(255,255,255)",
             boxShadow: "0px 0px 8px rgb(255,255,255)",
-
           }}
-
-          href="#" className="font-medium hover:text-primary">
-          Service
+          href="#"
+          className="font-medium hover:text-primary"
+        >
+          Contact
         </motion.a>
       </div>
       <div className="flex gap-4">
-        <button className="hidden md:block ">
+        <a
+          href="https://www.facebook.com/yousuf.joy.5/"
+          className="hidden md:block "
+        >
           <FaFacebookF className="w-[20px] h-[20px] hover:text-primary" />
-        </button>
-        <button className="hidden md:block">
+        </a>
+        <a href="https://github.com/Yousufjoy" className="hidden md:block ">
           <FaGithub className="w-[20px] h-[20px] hover:text-primary" />
-        </button>
-        <button className="hidden md:block">
+        </a>
+        <a
+          href="https://www.linkedin.com/in/md-yousuf-52862524a/"
+          className="hidden md:block "
+        >
           <FaLinkedinIn className="w-[20px] h-[20px] hover:text-primary" />
-        </button>
+        </a>
       </div>
-
-
-
     </motion.nav>
   );
 };
